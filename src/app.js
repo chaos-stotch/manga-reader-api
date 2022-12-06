409f84604b9de21b449bdadd3a34e87ae38fc2f1
Vitnin <chaos_stotch@proton.me>
2022-12-06 18:18:00 -0300

    localhost cross origin error fixed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const router = require('./router');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.use(express.json());
 app.use(router);
 
